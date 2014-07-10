2e37d083cd47b7312e0791a7f8c274d1b9e7b563
Takashi Ogura <t.ogura@gmail.com>
2014-07-11 00:54:23 +0900

    Imported upstream version '0.0.4' of 'upstream'

--- src/rqt_ez_publisher/ez_publisher_model.py
+++ src/rqt_ez_publisher/ez_publisher_model.py
@@ -7,16 +7,27 @@ from functools import reduce
 
 from rqt_py_common.topic_helpers import get_field_type
 
-# initial string must be topic name for get_field_type
+
+def get_field_type_capable_with_index(field_string):
+    m = re.search('(.+)(\[[0-9]+\])', field_string)
+    if m:
+        return get_field_type(m.group(1))
+    else:
+        return get_field_type(field_string)
+
 def make_topic_strings(msg_instance, string=''):
+    if msg_instance is None:
+        return string
     if isinstance(msg_instance, list):
-        array_instance = get_field_type(string)[0]()
+        array_instance = get_field_type_capable_with_index(string)[0]()
         return make_topic_strings(array_instance, string + '[0]')
     try:
-        return [make_topic_strings(msg_instance.__getattribute__(slot), string + '/' + slot)
+        return [make_topic_strings(msg_instance.__getattribute__(slot),
+                                   string + '/' + slot)
                 for slot in msg_instance.__slots__]
-    except AttributeError as e:
+    except AttributeError:
         return string
+
 
 def set_msg_attribute_value(msg_instance, topic_name, type, attributes,
                             array_index, value):
@@ -27,19 +38,20 @@ def set_msg_attribute_value(msg_instance, topic_name, type, attributes,
             full_string += '/' + attr
             m = re.search('(\w+)\[([0-9]+)\]', attr)
             if m:
-                array_type = get_field_type(full_string)[0]
                 index = int(m.group(2))
-                while len(message_target.__getattribute__(m.group(1))) <= index:
-                    message_target.__getattribute__(m.group(1)).append(array_type())
-                message_target = message_target.__getattribute__(m.group(1))[index]
-            elif get_field_type(full_string)[1]: # this is impossible
-                array_type = get_field_type(full_string)[0]
+                attr = m.group(1)
+                array_type = get_field_type_capable_with_index(full_string)[0]
+                while len(message_target.__getattribute__(attr)) <= index:
+                    message_target.__getattribute__(attr).append(array_type())
+                message_target = message_target.__getattribute__(attr)[index]
+            elif get_field_type_capable_with_index(full_string)[1]:  # this is impossible
+                array_type = get_field_type_capable_with_index(full_string)[0]
                 if len(message_target.__getattribute__(attr)) == 0:
                     message_target.__getattribute__(attr).append(array_type())
                 message_target = message_target.__getattribute__(attr)[0]
             else:
                 message_target = message_target.__getattribute__(attr)
-    if array_index != None:
+    if array_index is not None:
         array = message_target.__getattribute__(attributes[-1])
         while len(array) <= array_index:
             array.append(type())
@@ -65,7 +77,7 @@ def find_topic_name(full_text, topic_dict):
         return (full_text, None, None)
     splited_text = full_text.split('/')[1:]
     topic_name = ''
-    while not topic_name in topic_dict and splited_text:
+    while topic_name not in topic_dict and splited_text:
         topic_name += '/' + splited_text[0]
         splited_text = splited_text[1:]
     if splited_text:
@@ -87,7 +99,6 @@ def get_value_type(topic_type_str, attributes):
     try:
         _, spec = roslib.msgs.load_by_type(topic_type_str)
     except roslib.msgs.MsgSpecException as e:
-        print e
         return (None, False)
     try:
         head_attribute = attributes[0].split('[')[0]
@@ -97,7 +108,7 @@ def get_value_type(topic_type_str, attributes):
         if field.is_builtin:
             if attr_type in ['int8', 'int16', 'int32', 'int64']:
                 return (int, field.is_array)
-            if attr_type in ['uint8', 'uint16', 'uint32', 'uint64']:
+            if attr_type in ['byte', 'uint8', 'uint16', 'uint32', 'uint64']:
                 return ('uint', field.is_array)
             elif attr_type in ['float32', 'float64']:
                 return (float, field.is_array)
@@ -105,6 +116,9 @@ def get_value_type(topic_type_str, attributes):
                 return (str, field.is_array)
             elif attr_type == 'bool':
                 return (bool, field.is_array)
+            else:
+                print 'not support %s' % attr_type
+                return (None, False)
         else:
             return get_value_type(field.base_type, attributes[1:])
     except ValueError as e:
@@ -114,7 +128,7 @@ def get_value_type(topic_type_str, attributes):
 
 def make_text(topic_name, attributes, array_index):
     text = topic_name + '/' + '/'.join(attributes)
-    if array_index != None:
+    if array_index is not None:
         text += '[%d]' % array_index
     return text
 
@@ -154,16 +168,9 @@ class EasyPublisherModel(object):
             return None
 
     def _add_publisher_if_not_exists(self, topic_name, message_class):
-        if not topic_name in self._publishers:
+        if topic_name not in self._publishers:
             self._publishers[topic_name] = self._publisher_class(
                 topic_name, message_class)
-
-    def get_topic_break_down_strings(self, topic_name):
-        if topic_name in self._publishers:
-            return flatten(make_topic_strings(
-                self._publishers[topic_name].get_message(), topic_name))
-        else:
-            return []
 
     def get_topic_names(self):
         _, _, topic_types = rospy.get_master().getTopicTypes()
@@ -172,15 +179,18 @@ class EasyPublisherModel(object):
     def expand_attribute(self, input_text, array_index=None):
         text = copy.copy(input_text)
         try:
-            if get_field_type(text)[1]:
-                if array_index == None:
-                    index = 0
+            msg_type, is_array = get_field_type_capable_with_index(text)
+            if is_array:
+                if array_index is None:
+                    text += '[0]'
                 else:
-                    index = array_index
-                text += '[%s]'%index
-            type = get_field_type(text)[0]
-            if type:
-                return flatten(make_topic_strings(type(), text))
+                    array_string = '[%d]' % array_index
+                    if not text.endswith(array_string):
+                        text += array_string
+            if msg_type == int: # for time ? not support
+                return []
+            elif msg_type:
+                return flatten(make_topic_strings(msg_type(), text))
             else:
                 return []
         except AttributeError as e:
